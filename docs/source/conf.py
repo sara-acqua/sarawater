8407ff6b3f53954b0d60033d6ec54dd5235b0bd0
matteodalsanto <matteo.dalsanto@studenti.unitn.it>
2026-01-14 10:41:00 +0100

    user guide: add the description of Qrel and Qrule

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,9 @@ author = "Gabriele Barile, Matteo Dal Santo, Marta Crivellaro"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.mathjax',
+]
 
 templates_path = ["_templates"]
 exclude_patterns = []
